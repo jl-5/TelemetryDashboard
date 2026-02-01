efc277ffab4d8169c2984379024303b52d6dd8e7
Justin <144493322+jl-5@users.noreply.github.com>
2026-02-01 15:57:23 -0800

    create mock API for telemetry data

---
+++ dashboard/src/api/mockMetrics.ts
@@ -0,0 +1,23 @@
+import type { Metric } from "../types/metric";
+
+// currently we don't have the option to "reject" because this mock API only simulates successful data retrieval
+export function fetchMetrics(): Promise<Metric[]> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([
+                {
+                    id: "cpu",
+                    label: "CPU Usage",
+                    value: 47, 
+                    unit: "%",
+                },
+                {
+                    id: "memory",
+                    label: "Memory Usage",
+                    value: 74,
+                    unit: "%",
+                },
+            ]);
+        }, 500);
+    });
+}
